43ae0a8aacce709faa12361e73ec71d2ac45e3cd
Martin Gruscher <m.gruscher@ttech.at>
2018-04-01 20:25:29 +0200

    tidy up the build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
-g++ src\main.cpp -Id:/Projects/C++/libs/sdl2_mingw/SDL2-2.0.8/x86_64-w64-mingw32/include -Ld:/Projects/C++/libs/sdl2_mingw/SDL2-2.0.8/x86_64-w64-mingw32/lib -ID:/Projects/C++/libs/sdl2_mingw/SDL2_ttf-2.0.14/x86_64-w64-mingw32/include -LD:/Projects/C++/libs/sdl2_mingw/SDL2_ttf-2.0.14/x86_64-w64-mingw32/lib -lSDL2 -lSDL2main -lSDL2_ttf -o build\game.exe
+g++ src\main.cpp^
+	 -Id:/Projects/C++/libs/sdl2_mingw/SDL2-2.0.8/x86_64-w64-mingw32/include^
+	 -LD:/Projects/C++/libs/sdl2_mingw/SDL2-2.0.8/x86_64-w64-mingw32/lib^
+	 -ID:/Projects/C++/libs/sdl2_mingw/SDL2_ttf-2.0.14/x86_64-w64-mingw32/include^
+	 -LD:/Projects/C++/libs/sdl2_mingw/SDL2_ttf-2.0.14/x86_64-w64-mingw32/lib^
+	 -lSDL2 -lSDL2main -lSDL2_ttf^
+	 -o build\game.exe
